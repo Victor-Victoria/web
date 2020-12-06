e6cc7c0ef2b9685f57bdacff2c93574f554d1363
Victor-Victoria <71789485+Victor-Victoria@users.noreply.github.com>
2020-12-06 20:53:46 +0300

    Меню сайта и подвал

---
+++ Amour/frontend/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <header>
+      <div class="container">
+        <nav class="blok-menu">
+          <div class="logo-box">
+            <h1 class="logo">
+              <router-link v-bind:to="{ name: 'Home' }">
+                <img src="images/logo/amour_8.png" alt="Amour" />
+              </router-link>
+            </h1>
+          </div>
+          <div class="navbar-inverse">
+            <ul class="menu">
+              <li>
+                <router-link v-bind:to="{ name: 'Catalog' }"
+                  >Каталог</router-link
+                >
+              </li>
+              <li>
+                <router-link v-bind:to="{ name: 'Delivery' }"
+                  >Доставка и возврат</router-link
+                >
+              </li>
+              <li>
+                <router-link v-bind:to="{ name: 'Contact' }"
+                  >Контакты</router-link
+                >
+              </li>
+              <li><span class="ti-search"></span></li>
+              <li>
+                <router-link v-bind:to="{ name: 'SignIn' }"
+                  ><span class="ti-user"></span
+                ></router-link>
+              </li>
+              <li><span class="ti-shopping-cart"></span></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>
+
+    <main role="main">
+      <router-view />
+    </main>
+
+    <footer class="container">
+      <hr class="featurette-divider" />
+      <p class="float-right"><a href="#">Вернуться наверх</a></p>
+      <p>
+        &copy; 2020 Amour, Inc. &middot;<a href="#">Конфиденциальность</a>
+        &middot; <a href="#">Условия</a>
+      </p>
+    </footer>
+  </div>
+</template>
+
+
