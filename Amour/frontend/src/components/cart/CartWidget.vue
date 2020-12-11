db11d868cb443ed0292634c6d9fa552cf6e54c65
Victor-Victoria <71789485+Victor-Victoria@users.noreply.github.com>
2020-12-12 01:25:56 +0300

    Перенесено в static

---
+++ Amour/frontend/src/components/cart/CartWidget.vue
@@ -0,0 +1,20 @@
+<template>
+  <router-link v-bind:to="{name: 'Cart'}">
+    <span v-if="cart.total === 0" class="ti-shopping-cart"></span>
+    <span v-if="cart.total > 0" class="ti-shopping-cart-full"> {{cart.total}} ₽</span>
+  </router-link>
+</template>
+
+<script>
+  import CartData from '@/components/cart/cart'
+  export default {
+    name: "CartWidget",
+    data() {
+      let data = CartData
+      data.load()
+      return {
+        cart: data
+      }
+    }
+  }
+</script>
