5d3637a7e16487dff26c2ad366b1faa6f4e7a00b
Victor-Victoria <71789485+Victor-Victoria@users.noreply.github.com>
2020-12-12 01:27:46 +0300

    Добавлен пользователь

---
+++ Amour/frontend/src/views/Profile.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container profile">
+    <h1 class="font-weight-normal mb-3">{{user.name}}</h1>
+    <h4 class="font-weight-normal">{{user.email}}</h4>
+    <hr>
+    <button class="btn btn-purple" v-on:click="logout">Выйти</button>
+  </div>
+</template>
+
+<script>
+  import User from '@/components/user/user'
+  export default {
+    name: "Profile",
+    data() {
+      return {
+        user: User
+      }
+    },
+    methods: {
+      logout() {
+        User.logout()
+        //location.reload()
+      }
+    }
+  }
+</script>
